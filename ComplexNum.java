029929afe0e8037cc8f88653fa05c67f9cef0c49
Семён Курочкин <sema.kurochkin@mail.ru>
2023-07-27 15:42:48 +0300

    ComplexNum

---
+++ ComplexNum.java
@@ -0,0 +1,17 @@
+class ComplexNum {
+    public double x; //Действительная часть (коэфф a)
+    public double y; //Мнимая часть (коэфф b)
+
+    ComplexNum(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+}
